f85449e093884942427b6d62d5784310d5ddcb15
salvatore-ardisi <salvatore.ardisi@gmail.com>
2021-07-11 18:14:29 -0400

    task list

--- Taskdoro/app/src/main/java/com/example/taskdoro/Tasks.java
+++ Taskdoro/app/src/main/java/com/example/taskdoro/Tasks.java
@@ -1,12 +1,17 @@
 package com.example.taskdoro;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Tasks extends Fragment {
+    //private FloatingActionButton myButton;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +66,9 @@ public class Tasks extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+        View view = inflater.inflate(R.layout.fragment_tasks, container, false);
+
+
+        return view;
     }
 }
